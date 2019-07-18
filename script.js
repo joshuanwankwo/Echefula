a27e0d254d464caa15c3164c127328e4305ffeb5
Systemblack007 <systemblakes@gmail.com>
2019-07-18 19:13:16 +0100

    I just added the delete button

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-
+let arr = [];
 //adds a click event listener to the add button 
 document.querySelector(".btn").addEventListener("click", addItem);
+
+
+
 
 function addItem(){
     //reads the value from the input
     let currVal = document.querySelector('input').value;
-    if(currVal != '') {
-        //creates a list item element
-    let currItem = document.createElement('li');
-    //sets the text content of the list item to the current value of the input
-    currItem.textContent = currVal;
- 
+    if(currVal != '') { 
     //inserts the list item created before the end of the ul element
-    document.querySelector("#list").insertAdjacentElement("beforeend",currItem);
-    //clears the input
-    document.querySelector('input').value = '';
+    arr.push(currVal)
+    display()
+    document.querySelector('input').value = ""
     }
-    
+}
+
+
+
+function display(){
+  const li = document.createElement('li')
+  const Button =  document.createElement('button')
+  const ul = document.getElementById('list')
+  Button.textContent = "DONE"
+   Button.addEventListener('click',function(){
+       li.remove()
+   })
+
+  arr.forEach(function(todo){
+      li.textContent = todo;
+      li.appendChild(Button)
+      ul.appendChild(li)
+  })
+
 }
